43512d9f74df43eaadca08f82ef84a6f9a2d5583
Dhanashri Kokate <54618477+dhanashri97@users.noreply.github.com>
2022-05-27 13:55:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Image } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image src="https://www.publicdomainpictures.net/pictures/230000/velka/red-eared-slider-turtle.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
